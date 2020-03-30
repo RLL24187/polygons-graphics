ab24c01c25a239ae66e103530b4364cf3d1812d2
rleongPd5 <rleong00@stuy.edu>
2020-03-30 04:00:37 -0400

    having issues with runtime...

--- gmath.py
+++ gmath.py
@@ -25,7 +25,12 @@ def calculate_normal(polygons, i):
     p0 = polygons[i]
     p1 = polygons[i + 1]
     p2 = polygons[i + 2]
-    a = [p1[0] - p0[0], p1[1] - p0[1], p1[2] - p0[2]]
-    b = [p2[0] - p0[0], p2[1] - p0[1], p2[2] - p0[2]]
-    norm = [a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0]]
-    return norm
+    a = [p1[0] - p0[0],
+         p1[1] - p0[1],
+         p1[2] - p0[2]]
+    b = [p2[0] - p0[0],
+         p2[1] - p0[1],
+         p2[2] - p0[2]]
+    return [a[1] * b[2] - a[2] * b[1],
+            a[2] * b[0] - a[0] * b[2],
+            a[0] * b[1] - a[1] * b[0]]

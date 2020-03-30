a3a101f7551eb40f81ba086be1a235f85017c14a
rleongPd5 <rleong00@stuy.edu>
2020-03-30 01:15:23 -0400

    added dot_product

--- gmath.py
+++ gmath.py
@@ -4,13 +4,20 @@ from display import *
 
 
 #vector functions
-#normalize vetor, should modify the parameter
+#normalize vector, should modify the parameter
 def normalize(vector):
     pass
 
 #Return the dot porduct of a . b
 def dot_product(a, b):
-    return 0
+    x0 = a[0]
+    y0 = a[1]
+    z0 = a[2]
+    x1 = b[0]
+    y1 = b[1]
+    z1 = b[2]
+    dp = x0 * x1 + y0 * y1 + z0 * z1
+    return dp
 
 #Calculate the surface normal for the triangle whose first
 #point is located at index i in polygons
